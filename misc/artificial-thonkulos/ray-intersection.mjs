309ec04b0c509d88f87d9ae1f7590300ca7cd0ee
SheepTester <ussa.say@gmail.com>
2020-01-02 14:36:14 -0800

    Inelegantly get closest point on polyline to another point

--- misc/artificial-thonkulos/ray-intersection.mjs
+++ misc/artificial-thonkulos/ray-intersection.mjs
@@ -1,4 +1,6 @@
-export function raySegmentIntersection (origin, rayDir, a, b) {
+import { Vector2 } from './vectors.mjs'
+
+export function segmentIntersection (origin, rayDir, a, b, isRay = false) {
   // Ray (for t_r >= 0):
   // <x, y> = origin + rayDir * t_r
   // Segment (for t_s: [0, 1]):
@@ -24,19 +26,24 @@ export function raySegmentIntersection (origin, rayDir, a, b) {
   // Recall from before that
   // t_r = (a.x + segmentDir.x * t_s - origin.x) / rayDir.x
   const rayT = (a.x + segmentDir.x * segmentT - origin.x) / rayDir.x
-  // If t_r < 0, then the intersection was not on the ray
-  if (rayT < 0) return null
+  // If t_r < 0, then the intersection was not on the ray (if it's a ray)
+  if (isRay && rayT < 0) return null
   // Get point at t_r
-  return origin.clone().add(rayDir.clone().scale(rayT))
+  return {
+    segmentT,
+    rayT,
+    point: origin.clone().add(rayDir.clone().scale(rayT))
+  }
 }
 
-export function rayPolygonIntersection (origin, direction, polygon) {
+export function rayPolylineIntersection (origin, direction, polyline) {
   let closestIntersection = null
   let closestDistance = Infinity
-  for (let i = 1; i < polygon.length; i++) {
-    if (polygon[i - 1] && polygon[i]) {
-      const point = raySegmentIntersection(origin, direction, polygon[i - 1], polygon[i])
-      if (!point) continue
+  for (let i = 1; i < polyline.length; i++) {
+    if (polyline[i - 1] && polyline[i]) {
+      const intersection = segmentIntersection(origin, direction, polyline[i - 1], polyline[i], true)
+      if (!intersection) continue
+      const { point } = intersection
       const distance = point.clone().sub(origin).length
       if (distance < closestDistance) {
         closestDistance = distance
@@ -45,4 +52,56 @@ export function rayPolygonIntersection (origin, direction, polygon) {
     }
   }
   return closestIntersection
+}
+
+/**
+ * Get closest point in a polyline to a point
+ */
+export function getClosestToPolyline (point, polyline) {
+  let closest = { distance: Infinity }
+  let segments = 0
+  for (let i = 1; i < polyline.length; i++) {
+    const a = polyline[i - 1]
+    const b = polyline[i]
+    if (a && b) {
+      const normal = new Vector2(a.y - b.y, b.x - a.x)
+      const intersection = segmentIntersection(point, normal, a, b, false)
+      if (intersection) {
+        const {
+          segmentT,
+          point: intersectionPoint
+        } = intersection
+        const distance = intersectionPoint.clone().sub(point).length
+        if (distance < closest.distance) {
+          closest = {
+            t: segments + segmentT,
+            point: intersectionPoint,
+            distance
+          }
+        }
+      } else {
+        // Try using the endpoints of the segment if the normal doesn't intersect
+        // the segment
+        const distanceA = a.clone().sub(point).length
+        if (distanceA < closest.distance) {
+          closest = {
+            t: segments,
+            point: a.clone(),
+            distance: distanceA
+          }
+          // Continue to check B in case it's closer
+        }
+        const distanceB = b.clone().sub(point).length
+        if (distanceB < closest.distance) {
+          closest = {
+            t: segments,
+            point: b.clone(),
+            distance: distanceB
+          }
+        }
+      }
+      segments++
+    }
+  }
+  return closest.point ? closest : null
 }
