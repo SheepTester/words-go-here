69e10679cf1148d1c8e39bbf5eb24e39b588993f
SheepTester <ussa.say@gmail.com>
2020-07-08 18:05:48 -0700

    Block grid map and tooltips

--- misc/storage-room-a-star.js
+++ misc/storage-room-a-star.js
@@ -26,4 +26,54 @@ export class Board {
       this.set(x, y, value)
     }
   }
+
+  getLocation (value) {
+    const index = this._board.indexOf(value)
+    return index < 0 ? null : [index % this.width, Math.floor(index / this.width)]
+  }
+}
+
+export class ElemBoard extends Board {
+  constructor (sourceBoard) {
+    super(sourceBoard.width, sourceBoard.height)
+
+    const wrapper = document.createElement('div')
+    wrapper.className = 'board'
+    wrapper.style.setProperty('--width', this.width)
+    wrapper.style.setProperty('--height', this.height)
+    this._wrapper = wrapper
+
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        const cell = sourceBoard.get(x, y)
+        if (cell) {
+          const cellElem = document.createElement('div')
+          cellElem.className = 'cell'
+          cellElem.style.backgroundImage = `url("${cell.texture}")`
+          cellElem.style.setProperty('--x', x)
+          cellElem.style.setProperty('--y', y)
+          cellElem.dataset.x = x
+          cellElem.dataset.y = y
+          wrapper.appendChild(cellElem)
+          this.set(x, y, cellElem)
+        }
+      }
+    }
+  }
+
+  moveTo (x, y) {
+    this._wrapper.style.setProperty('--x', x)
+    this._wrapper.style.setProperty('--y', y)
+    return this
+  }
+
+  addTo (parent) {
+    parent.appendChild(this._wrapper)
+    return this
+  }
+
+  setTag (tag) {
+    this._wrapper.dataset.tag = tag
+    return this
+  }
 }
