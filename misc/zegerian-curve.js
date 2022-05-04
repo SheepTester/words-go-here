f390de2a59210edc87a67694cc57ec6dfacea154
Sean <ussa.say@gmail.com>
2022-05-03 17:21:11 -0700

    zeger: Histogram bar chart

--- misc/zegerian-curve.js
+++ misc/zegerian-curve.js
@@ -1,10 +1,13 @@
 import { h, Fragment, render } from 'preact'
 import { useEffect, useState, useRef } from 'preact/hooks'
 
-function Quiz ({ name, scores }) {
+function Quiz ({ name, scores, afterCurrent, onSelect }) {
   return h(
     'div',
-    { class: 'quiz' },
+    {
+      class: `quiz ${afterCurrent ? 'after' : ''}`,
+      onMouseEnter: onSelect
+    },
     h('h3', { class: 'quiz-name' }, name),
     h(
       'div',
@@ -17,19 +20,27 @@ function Quiz ({ name, scores }) {
             title: `${score}/3`,
             style: { flexGrow: count }
           },
-          count
+          h('span', { class: 'score-number' }, count)
         )
       )
     )
   )
 }
 
-function Quizzes ({ quizzes }) {
+function Quizzes ({ quizzes, quiz, onQuiz }) {
   return h(
     'div',
-    { class: 'quizzes' },
+    { class: 'quizzes', onMouseLeave: onQuiz && (() => onQuiz(null)) },
     h('h2', { class: 'heading quizzes-heading' }, 'Quiz scores'),
-    quizzes.map((scores, i) => h(Quiz, { name: `Quiz ${i + 1}`, scores })),
+    quizzes.map((scores, i) =>
+      h(Quiz, {
+        key: i,
+        name: `Quiz ${i + 1}`,
+        scores,
+        afterCurrent: quiz !== null && i + 1 > quiz,
+        onSelect: onQuiz && (() => onQuiz(i + 1))
+      })
+    ),
     h(
       'div',
       { class: 'key' },
@@ -37,7 +48,7 @@ function Quizzes ({ quizzes }) {
       [0, 1, 2, 3].map(score =>
         h(
           'div',
-          { class: 'score-key' },
+          { class: 'score-key', key: score },
           h('div', { class: `score-colour score-${score}` }),
           ' ',
           score,
@@ -48,8 +59,49 @@ function Quizzes ({ quizzes }) {
   )
 }
 
+function Histogram ({ scores, showingAll }) {
+  const maxCount = Math.max(...scores)
+
+  return h(
+    'div',
+    { class: `histogram ${showingAll ? 'showing-all' : ''}` },
+    scores.map((count, score) =>
+      h('div', {
+        key: score,
+        class: 'bar',
+        style: { height: `${(count / maxCount) * 100}%` }
+      })
+    )
+  )
+}
+
+function Histograms ({ histogram, quiz }) {
+  const showAll = quiz === null || quiz < 2
+
+  return h(
+    'div',
+    { class: 'histograms' },
+    h(
+      'div',
+      { class: 'histogram-wrapper' },
+      histogram.map((scores, i) =>
+        showAll || i + 2 === quiz
+          ? h(Histogram, { scores, key: i, showingAll: showAll })
+          : null
+      )
+    )
+  )
+}
+
 function App ({ quizzes, histogram }) {
-  return h(Fragment, null, h(Quizzes, { quizzes }))
+  const [quiz, setQuiz] = useState(null)
+
+  return h(
+    Fragment,
+    null,
+    h(Quizzes, { quizzes, quiz, onQuiz: setQuiz }),
+    h(Histograms, { histogram, quiz })
+  )
 }
 
 render(
