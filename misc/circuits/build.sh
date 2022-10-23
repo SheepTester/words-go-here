4a59bbae0cddfbafe0d1992797af4bd5fcbd0ad5
Sean <ussa.say@gmail.com>
2022-10-22 19:56:46 -0700

    ece65: Convert pdfs to svgs

---
+++ misc/circuits/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd misc/circuits/
+rm -f *.aux
+rm -f *.fdb_latexmk
+rm -f *.fls
+rm -f *.log
+rm -f *.synctex.gz
+rm -f *.synctex\(busy\)
+
+for file in *.pdf
+do
+  svg="${file%.*}.svg"
+  pdf2svg $file $svg
+  sed -i \
+    -e 's/rgb(0%,0%,0%)/#94a3b8/' \
+    -e 's/rgb(100%,100%,100%)/#0f172a/' \
+    -e 's/rgb(100%,0%,0%)/white/' \
+    $svg
+done
