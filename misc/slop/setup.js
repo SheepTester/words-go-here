d3b6f27f9c378f944a84f21a43177e37db2fe845
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-06-06 01:01:34 -0700

    fix setup

--- misc/slop/setup.js
+++ misc/slop/setup.js
@@ -21,41 +21,39 @@ export async function setup ({ destroy, ...availabilityArgs }) {
   }
 
   status.className = 'normal'
-  if (availability === 'downloadable' || availability === 'available') {
-    status.style.display = 'flex'
-    if (availability === 'downloadable') {
-      status.innerHTML = [
-        '<h2>Your computer is pure</h2>',
-        '<p>A random website hasn\'t already silently downloaded a 4 GB LLM on your computer (since <a href="https://developer.chrome.com/docs/ai/prompt-api" class="link">Chrome lets them do that</a>), so this website will be the first. Press the button below when you\'re ready.</p>',
-        '<div class="button-row"><button type="button" class="button primary-btn" id="setup-download">Download four gigabytes</button></div>',
-        '<progress id="setup-progress" min="0" max="100" value="0">your browser doesnt support progress</progress>',
-        "<p>Tip: It can get stuck around 90%. I think it means it finished downloading the model, so it's moving the gigabytes around on your computer.</p>"
-      ].join('')
-      await new Promise(resolve =>
-        document
-          .getElementById('setup-download')
-          .addEventListener('click', resolve)
-      )
-    } else {
-      status.innerHTML = [
-        '<p>Getting ready...</p>',
-        '<progress id="setup-progress" min="0" max="100" value="0">your browser doesnt support progress</progress>'
-      ].join('')
+  status.style.display = 'flex'
+  if (availability === 'downloadable') {
+    status.innerHTML = [
+      '<h2>Your computer is pure</h2>',
+      '<p>A random website hasn\'t already silently downloaded a 4 GB LLM on your computer (since <a href="https://developer.chrome.com/docs/ai/prompt-api" class="link">Chrome lets them do that</a>), so this website will be the first. Press the button below when you\'re ready.</p>',
+      '<div class="button-row"><button type="button" class="button primary-btn" id="setup-download">Download four gigabytes</button></div>',
+      '<progress id="setup-progress" min="0" max="100" value="0">your browser doesnt support progress</progress>',
+      "<p>Tip: It can get stuck around 90%. I think it means it finished downloading the model, so it's moving the gigabytes around on your computer.</p>"
+    ].join('')
+    await new Promise(resolve =>
+      document
+        .getElementById('setup-download')
+        .addEventListener('click', resolve)
+    )
+  } else {
+    status.innerHTML = [
+      '<p>Getting ready...</p>',
+      '<progress id="setup-progress" min="0" max="100" value="0">your browser doesnt support progress</progress>'
+    ].join('')
+  }
+  const progress = document.getElementById('setup-progress')
+  const session = await LanguageModel.create({
+    ...availabilityArgs,
+    monitor: monitor => {
+      monitor.addEventListener('downloadprogress', e => {
+        progress.value = e.loaded * 100
+      })
     }
-    const progress = document.getElementById('setup-progress')
-    const session = await LanguageModel.create({
-      ...availabilityArgs,
-      monitor: monitor => {
-        monitor.addEventListener('downloadprogress', e => {
-          progress.value = e.loaded * 100
-        })
-      }
-    })
-    status.style.display = 'none'
-    if (destroy) {
-      session.destroy()
-    } else {
-      return session
-    }
+  })
+  status.style.display = 'none'
+  if (destroy) {
+    session.destroy()
+  } else {
+    return session
   }
 }
